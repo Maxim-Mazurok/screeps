e471d896f361527206b9ea1dc9a42b629d05b066
Maxim Mazurok <maxim@mazurok.com>
2019-04-06 09:41:20 +0300

    CreepRole

---
+++ build/enums.js
@@ -0,0 +1,13 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+var CreepRole;
+(function (CreepRole) {
+    CreepRole[CreepRole["harvester"] = 0] = "harvester";
+    CreepRole[CreepRole["builder"] = 1] = "builder";
+    CreepRole[CreepRole["upgrader"] = 2] = "upgrader";
+    CreepRole[CreepRole["extractor"] = 3] = "extractor";
+    CreepRole[CreepRole["energizer"] = 4] = "energizer";
+    CreepRole[CreepRole["uptownHarvester"] = 5] = "uptownHarvester";
+    CreepRole[CreepRole["claimer"] = 6] = "claimer";
+    CreepRole[CreepRole["uptownBuilder"] = 7] = "uptownBuilder";
+})(CreepRole = exports.CreepRole || (exports.CreepRole = {}));
+//# sourceMappingURL=enums.js.map
