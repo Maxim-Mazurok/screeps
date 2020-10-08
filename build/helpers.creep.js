c905dd4c80a775d7e4214393d8a50ce95be43ad8
Maxim Mazurok <maxim@mazurok.com>
2020-10-08 19:06:27 +1100

    limit body length

--- build/helpers.creep.js
+++ build/helpers.creep.js
@@ -55,6 +55,8 @@ class HelpersCreep {
             return [...body, part];
         }
         function tryToAddToBody(part) {
+            if (bodyParts.length >= 50)
+                return; // Should contain 1 to 50 elements
             const newPart = part ? part : bodyParts[lastBodyPartIndex];
             if (HelpersCreep.moveTimeByParts(newBody(newPart), terrainFactor) > 1) {
                 tryToAddToBody(MOVE);
