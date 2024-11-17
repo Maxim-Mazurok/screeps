88a357ac4fdbf7c602f235b86afc8f10f24f3044
Maxim Mazurok <maxim@mazurok.com>
2024-11-17 13:10:28 +1100

    better damage repair

--- build/towers.js
+++ build/towers.js
@@ -16,22 +16,38 @@ class Towers {
                 tower.attack(closestHostile);
             }
             else {
-                const closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        if (structure.structureType === STRUCTURE_WALL) {
-                            return structure.hits < maxWallHits;
-                        }
-                        return (structure.hits < structure.hitsMax && structure.hits < maxHits);
-                    },
-                });
                 const weakStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
                     filter: (structure) => structure.hits < structure.hitsMax && structure.hits < 100,
                 });
                 if (weakStructure) {
                     tower.repair(weakStructure);
                 }
-                else if (closestDamagedStructure) {
-                    tower.repair(closestDamagedStructure);
+                else {
+                    const damagedStructures = tower.room.find(FIND_STRUCTURES, {
+                        filter: (structure) => {
+                            if (structure.structureType === STRUCTURE_WALL) {
+                                return structure.hits < maxWallHits;
+                            }
+                            return structure.hits < structure.hitsMax && structure.hits < maxHits;
+                        },
+                    });
+                    const target = damagedStructures
+                        .map(structure => ({
+                        structure,
+                        damagePercent: structure.structureType === STRUCTURE_WALL
+                            ? 1 - structure.hits / maxWallHits
+                            : 1 - structure.hits / Math.min(structure.hitsMax, maxHits),
+                        distance: tower.pos.getRangeTo(structure)
+                    }))
+                        .sort((a, b) => {
+                        if (Math.abs(a.damagePercent - b.damagePercent) > 0.2) {
+                            return b.damagePercent - a.damagePercent;
+                        }
+                        return a.distance - b.distance;
+                    })[0];
+                    if (target) {
+                        tower.repair(target.structure);
+                    }
                 }
             }
         });
